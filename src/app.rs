63b8d342c1d88440bf45b56c363add00fe521163
Niki123456123456 <85129772+Niki123456123456@users.noreply.github.com>
2024-04-20 14:21:12 +0200

    add languages

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,47 @@
+use std::sync::mpsc::SyncSender;
 
+use egui::{Color32, Frame};
 
 #[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] 
+#[serde(default)]
+#[derive(Default)]
+pub struct Choice {
+    pub title: String,
+    pub explanation: String,
+}
+
+#[derive(Clone,Default, serde::Deserialize, serde::Serialize)]
+pub enum Rating {
+    #[default] Neutral,
+    Up,
+    Down,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+#[derive(Default)]
+pub struct Criteria {
+    pub title: String,
+    pub choices: Vec<Choice>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
 pub struct App {
+    #[serde(skip)]
+    pub criterias: Vec<Criteria>,
+    pub selected_criteria: usize,
+    pub answers: Vec<Vec<Rating>>,
 }
 
 impl Default for App {
     fn default() -> Self {
+        let criterias: Vec<Criteria> =
+            serde_json::from_slice(include_bytes!("../assets/criteria.json")).unwrap();
         Self {
+            criterias,
+            selected_criteria: 0,
+            answers: vec![],
         }
     }
 }
@@ -27,12 +61,68 @@ impl eframe::App for App {
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if self.selected_criteria >= self.criterias.len() {
+            self.selected_criteria = 0;
+        }
+        if self.answers.len() != self.criterias.len() {
+            self.answers = vec![vec![Rating::Neutral]; self.criterias.len()];
+        }
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("code o mat");
+            if let Some(criteria) = self.criterias.get(self.selected_criteria) {
+                let answers = self.answers.get_mut(self.selected_criteria).unwrap();
+                if answers.len() != criteria.choices.len() {
+                    *answers = vec![Rating::Neutral; self.criterias.len()];
+                }
 
-           
+                ui.label(format!(
+                    "question {}/{}",
+                    self.selected_criteria,
+                    self.criterias.len() - 1
+                ));
+                ui.heading(&criteria.title);
+                for (i, choice) in criteria.choices.iter().enumerate() {
+                    ui.horizontal(|ui| {
+                        let color = match answers[i] {
+                            Rating::Up => Color32::DARK_GREEN,
+                            Rating::Down => Color32::DARK_RED,
+                            Rating::Neutral => Color32::DARK_GRAY,
+                        };
+                        let group = Frame::group(ui.style()).fill(color).show(ui, |ui| {
+                            ui.vertical(|ui| {
+                                ui.heading(&choice.title);
+                                ui.label(&choice.explanation);
+                            });
+                        });
+                        if ui
+                            .interact(group.response.rect, ui.next_auto_id(), egui::Sense::click())
+                            .clicked()
+                        {
+                            let ans = match answers[i] {
+                                Rating::Up => Rating::Down,
+                                Rating::Down => Rating::Neutral,
+                                Rating::Neutral => Rating::Up,
+                            };
+                            self.answers[self.selected_criteria][i] = ans;
+                        }
+                    });
+                }
+            }
+        });
+
+        egui::TopBottomPanel::bottom("bottom").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if ui.button("previous").clicked() {
+                    if self.selected_criteria > 1 {
+                        self.selected_criteria -= 1;
+                    }
+                }
+                if ui.button("next").clicked() {
+                    if self.selected_criteria < self.criterias.len() - 1 {
+                        self.selected_criteria += 1;
+                    }
+                }
+            });
         });
     }
 }
-
